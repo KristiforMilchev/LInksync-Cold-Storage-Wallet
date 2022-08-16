8043d85571a0c7f90a643617d306da146ebebb60
Kristifor <kristifor@collaborativesoft.com>
2022-08-16 12:50:58 +0300

    Code refactoring, adding comments to methods.

--- Services/Definitions/IAuthenicationService.cs
+++ Services/Definitions/IAuthenicationService.cs
@@ -10,17 +10,40 @@ namespace SYNCWallet.Services.Definitions
 {
     public interface IAuthenicationService
     {
-        public string CreateAccountInitial();
-        public Task<string> DeployContract();
+        /// <summary>
+        /// Gets the public key of the connected wallet.
+        /// </summary>
         public string GetDefault();
+        /// <summary>
+        /// Impoirts a custom EVM compatible blockchain network
+        /// Parameters:
+        /// <param name="networkName">Name of the network</param>            
+        /// <param name="networkSymbol">SYMBOL of the network</param>        
+        /// <param name="rpcUrl">rpc endpoint of the network</param>        
+        /// <param name="chainID">chain idof the network</param>        
+        /// <param name="blockExplorer">https block explorer</param>        
+        /// </summary>
         public bool SetupNetwork(string networkName, string networkSymbol, string rpcUrl, int chainID, string blockExplorer);
+        /// <summary>
+        /// Imports custom token not listed on the official list.
+        /// Parameters:
+        /// <param name="contractAddress">Address of the contract that we import </param>        
+        /// <param name="symbol">Token Symbol</param>        
+        /// <param name="delimiter">Decimal count of the token</param>        
+        /// <param name="network">Internal network id of the network under which we import the token</param>        
+        /// </summary>
         public bool ImportToken(string contractAddress, string symbol, int delimiter, int network);
+        /// <summary>
+        /// Attepmts to decrypt and unlock a web3 wallet using a password
+        /// Parameters:
+        /// <param name="pass">Password used to encrypt the wallet.</param>        
+        /// </summary>
         public Account UnlockWallet(string pass);
+        /// <summary>
+        /// Gets a list of supported tokens filtered by network id
+        /// Parameters:
+        /// <param name="networkId">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// </summary>
         public Task<List<Token>> GetSupportedTokens(int networkId);
-        string ToHex(byte[] value, bool prefix = false);
-        byte ConvertBinaryToText(string seq);
-
-
-
     }
 }
--- Services/Definitions/IContractService.cs
+++ Services/Definitions/IContractService.cs
@@ -11,25 +11,132 @@ namespace SYNCWallet.Services.Definitions
 {
     public interface IContractService
     {
-        public Task<decimal> GetAccountBalance(int network, string endpoint);
+        /// <summary>
+        /// Gets the native token balance of a wallet based on the supplied network RPC endpoint
+        /// Parameters:
+        /// <param name="endpoint">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// </summary>
+        public Task<decimal> GetAccountBalance(string endpoint);
+
+        /// <summary>
+        /// This is a base method for a price converter, it's main purpose is to convert the input amount of tokens from the native token to USD
+        /// In order to perform the conversion it creates the call to the trading router taking the value of 1 of the pair token to USD 
+        /// Then multiply the tokens over the price of 1 Native token to USD
+        /// 
+        /// Example 1 BNB at the time of writing this method 262 USD if we input 0.00000000000032 bnb we will get a conversion rate of 0.00000000008384 USD
+        /// 
+        /// Parameters:
+        /// <param name="tokens">The input amount of tokens that has to be converted, example 0.0000000032 bnb to usd</param>
+        /// <param name="addresses">Array of contract addresses, first is the token that we are converting, second contract is in the currency that we are converting to.</param>
+        /// <param name="endpoint">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// <param name="router">Public contract address of the router where we want to check the price.</param>
+        /// </summary>
         public Task<decimal> ConvertTokenToUsd(decimal tokens, string[] addresses, string endpoint, string router);
+
+        /// <summary>
+        /// Interacts with a solidity smart contract to get the balance of the selected public address
+        /// Parameters:
+        /// <param name="ownerAddress">The public address that we want to check</param>
+        /// <param name="contract">The contract address that we want to run the check against.</param>
+        /// <param name="endpoint">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// <param name="decimals">Delimiter of the decimals in case the contract has less then 18 decimals.</param>       
+        /// </summary>
         public Task<decimal> CheckUserBalanceForContract(string ownerAddress, string contract, string endpoint, int decimals);
+
         //TODO implement in V2
         //public Task<string> MintToken(); 
+
+        /// <summary>
+        /// Gets the user imported and officially listed tokens for a given network
+        /// Parameters:
+        /// <param name="networkId">Internal ID of the selected network </param>        
+        /// </summary>
         public Task<List<SYNCWallet.Models.Token>> GetNetworkTokens(int networkId);
-        public Task<bool> ExecutePayments(string receiver, TokenContract token, decimal amountToSend, Nethereum.Web3.Accounts.Account account, string endpoint, int chainId);
-        public Task<bool> ExecuteNative(string receiver, decimal amountToSend, Nethereum.Web3.Accounts.Account account, string endpoint, int chainId);
+
+        /// <summary>
+        /// Checks the price of a given contract
+        /// Parameters:
+        /// <param name="contractAddress">The address of the router that holds the 2 pairs</param>
+        /// <param name="token">The contract address.</param>
+        /// <param name="pair">The contract liquidity pair.</param>
+        /// <param name="endpoint">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// <param name="pairDecimals">Delimiter of the decimals in case the contract has less then 18 decimals.</param>          
+        /// </summary>
         public Task<decimal> CheckContractPrice(string contractAddress, string token, string pair, int tokenDecimals, int pairDecimals, string endpoint);
 
-
-
-        public Task<decimal> GetImportedData(NetworkSettings network, TokenContract getContract);
-        public Task<List<Token>> GetListedTokens(List<ListedToken> listedTokenData, List<Token> tokens, NetworkSettings network);
+        /// <summary>
+        /// Gets the market cap and the ciruclating supply of a token.
+        /// Parameters:
+        /// <param name="supply">The total supply of the contract</param>
+        /// <param name="getTokenPrice">Current price of the token in USD</param>
+        /// <param name="contractAddress">The contract that we try to find the market capital and circulating supply</param>
+        /// <param name="endpoint">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// <param name="decimals">Delimiter of the decimals in case the contract has less then 18 decimals.</param>          
+        /// </summary>
         public Task<(decimal, decimal)> GetContractMarketCap(decimal supply, decimal getTokenPrice, string contractAddress, string endpoint, int decimals);
+
+        /// <summary>
+        /// Check if a contract is listed on the supported defi exchange
+        /// Parameters:
+        /// <param name="contractAddress">The address of the contract that we try to check</param>
+        /// <param name="pairToken">The liquidity pair</param>
+        /// <param name="endpoint">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// <param name="factory">The address of the exchange</param>          
+        /// </summary>
         public Task<string> CheckExchangelisting(string contractAddress, string pairToken, string endpoint, string factory);
+
+
+        /// <summary>
+        /// Gets the total supply of a given asset.
+        /// Parameters:
+        /// <param name="contractAddress">The address of the contract that we try to check</param>
+        /// <param name="endpoint">Blockchain endpoint, RPC address of the Blockchain network </param>        
+        /// <param name="tokenDecimals">Decimal points of the contract that we check</param>          
+        /// </summary>
         public Task<decimal> CheckExistingSupply(string contractAddress, string endpoint, int tokenDecimals);
 
 
+        /// <summary>
+        /// Internal method that calls CheckUserBalanceForContract so it can retrive token information for mcap, supply and price.
+        /// Parameters:
+        /// <param name="network">Selected network object</param>
+        /// <param name="getContract">imported token object.</param>
+        /// </summary>
+        public Task<decimal> GetImportedData(NetworkSettings network, TokenContract getContract);
+
+
+        /// <summary>
+        /// Returns a list of tokens from the official repository main branch.
+        /// Parameters:
+        /// <param name="network">Selected network object</param>
+        /// <param name="listedTokenData">Already listed assets collection</param>
+        /// <param name="tokens">The local collection of imported tokens</param>
+
+        /// </summary>
+        public Task<List<Token>> GetListedTokens(List<ListedToken> listedTokenData, List<Token> tokens, NetworkSettings network);
+
+        /// <summary>
+        /// Interacts with a smart contract and tries to transfer tokens from wallet address to another wallet address
+        /// Parameters:
+        /// <param name="receiver">The address that has to receive the funds</param>
+        /// <param name="token">Selected token object.</param>
+        /// <param name="amountToSend">The amount to send.</param>
+        /// <param name="account">Decrypted web3 wallet.</param>
+        /// <param name="endpoint">The network RPC endpoint where to send the request.</param>
+        /// <param name="chainId">The chain ID of the blockchain</param>
+        /// </summary>
+        public Task<bool> ExecutePayments(string receiver, TokenContract token, decimal amountToSend, Nethereum.Web3.Accounts.Account account, string endpoint, int chainId);
+
+        /// <summary>
+        /// Send native blockchain tokens to a given address
+        /// Parameters:
+        /// <param name="receiver">The address that has to receive the funds</param>
+        /// <param name="amountToSend">The amount to send.</param>
+        /// <param name="account">Decrypted web3 wallet.</param>
+        /// <param name="endpoint">The network RPC endpoint where to send the request.</param>
+        /// <param name="chainId">The chain ID of the blockchain</param>
+        /// </summary>
+        public Task<bool> ExecuteNative(string receiver, decimal amountToSend, Nethereum.Web3.Accounts.Account account, string endpoint, int chainId);
 
     }
 }
