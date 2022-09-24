ec777a2ee563e26a462fdeb3b3d422a6e9cf371f
kristiforMilchev <kristifor@collaborativesoft.com>
2022-09-24 11:12:21 +0300

    recovery

--- Initializer.cs
+++ Initializer.cs
@@ -14,6 +14,7 @@ namespace LInksync_Cold_Storage_Wallet
                 string codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
                 UriBuilder uri = new UriBuilder(codeBase);
                 string path = Uri.UnescapeDataString(uri.Path);
+                
                 return Path.GetDirectoryName(path);
             }
         }
