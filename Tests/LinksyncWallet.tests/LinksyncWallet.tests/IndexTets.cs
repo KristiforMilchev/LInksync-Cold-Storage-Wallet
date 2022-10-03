2f6ebba10a74b7029b434038ac81d10f2b7bfd4f
Kristifor Milchev <kristifor@collaborativesoft.com>
2022-10-03 10:48:41 +0300

    adding a test sample

--- Tests/LinksyncWallet.tests/LinksyncWallet.tests/IndexTets.cs
+++ Tests/LinksyncWallet.tests/LinksyncWallet.tests/IndexTets.cs
@@ -1,7 +1,15 @@
+using LibGit2Sharp;
+using Xunit;
+
 namespace LinksyncWallet.tests
 {
     public class IndexTets
     {
-        
+        [Fact]
+        public void LoadColdWallet_Test()
+        {
+          //  var index = new Index();
+            
+        }
     }
 }
